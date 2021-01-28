df728b61d8e226efd6e1048078dc504c54b08e52
jacos <cyrstem@gmail.com>
2021-01-28 18:32:35 -0500

    responsive

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./assets/HeroLG.svg":[["HeroLG.ca44cc60.svg","assets/HeroLG.svg"],"assets/HeroLG.svg"],"./assets/logo.svg":[["logo.b37d81ec.svg","assets/logo.svg"],"assets/logo.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./assets/HeroLG.svg":[["HeroLG.ca44cc60.svg","assets/HeroLG.svg"],"assets/HeroLG.svg"],"./assets/logo.svg":[["logo.b37d81ec.svg","assets/logo.svg"],"assets/logo.svg"],"./assets/Hero.svg":[["Hero.0a617fa1.svg","assets/Hero.svg"],"assets/Hero.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38045" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35987" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
